454b96ed86043037f124d698c3ff4fa21a584bc6
Maxwell Ray <mray02@calpoly.edu>
2019-05-18 15:35:15 -0700

    create observation form has been brought into redux-form, so everything can be managed through that instead of plain state.

--- src/components/create/fieldLeaders/FieldLeaderField.js
+++ src/components/create/fieldLeaders/FieldLeaderField.js
@@ -1,48 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {withStyles} from '@material-ui/core/styles';
-
+import {Field} from 'redux-form';
 import TextField from '@material-ui/core/TextField';
 import defaultStyles from '../../../defaultStyles';
 
-const styles = theme => {return {...defaultStyles(theme)}};
+const styles = theme => {
+    return {...defaultStyles(theme)}
+};
 
 class FieldLeaderField extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            key: props.keyId,
-            leader: '',
-            handleLeader: props.handleLeader
-        }
-    }
-
-    handleChange = (event) => {
-        this.setState({leader: event.target.value}, () => {
-            this.state.handleLeader(this.state.leader)
-        });
-    };
-
-    render() {
-        const {classes} = this.props;
+    renderFieldLeader({input: {name, value, onChange}, classes}) {
         return (
-            <div key={this.state.key} className={classes.formRow}>
+            <div className={classes.formRow}>
                 <TextField
                     id={"fieldLeader"}
                     label={"Field Leader"}
                     className={classes.textField}
-                    value={this.state.leader}
-                    onChange={this.handleChange}
+                    value={!value ? '' : value}
+                    onChange={(event) => onChange(event.target.value)}
                 />
             </div>
+        )
+    }
+
+    render() {
+        const {classes, name} = this.props;
+        return (
+            <Field name={name} component={this.renderFieldLeader} classes={classes}/>
         )
     }
 }
 
 FieldLeaderField.propTypes = {
     classes: PropTypes.object.isRequired,
-    keyId: PropTypes.number.isRequired,
-    handleLeader: PropTypes.func.isRequired
+    name: PropTypes.string.isRequired
 };
 
 export default withStyles(styles)(FieldLeaderField);
