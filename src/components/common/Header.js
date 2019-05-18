ee0945ba9818e156a755017c8460cf60447c7694
Maxwell Ray <me@maxray.me>
2019-05-18 15:38:58 -0700

    added link to title so that clicking it goes to cover page

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
+import {withStyles} from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import CreateModalButton from "../create/CreateObservationDialog";
+import {Link} from 'react-router-dom';
 
 const styles = {
     root: {
@@ -18,6 +19,10 @@ const styles = {
         marginLeft: -12,
         marginRight: 20,
     },
+    link: {
+        textDecoration: 'none',
+        color: 'inherit'
+    }
 };
 
 const Header = (props) => {
@@ -26,9 +31,11 @@ const Header = (props) => {
         <div className={classes.root}>
             <AppBar position={"static"}>
                 <Toolbar>
-                    <Typography variant={"h6"} color={"inherit"} className={classes.grow}>
-                        SeaQL
-                    </Typography>
+                    <Link to={'/'} className={[classes.link, classes.grow].join(' ')}>
+                        <Typography variant={"h6"} color={"inherit"}>
+                            SeaQL
+                        </Typography>
+                    </Link>
                     <CreateModalButton/>
                     <Button color={"inherit"}>Account</Button>
                     <Button color={"inherit"}>Logout</Button>
