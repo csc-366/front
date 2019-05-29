9b261e1eaa0d41a11f9b3b3158a620b67e26c96b
jccho16 <jccho@calpoly.edu>
2019-05-29 13:35:36 -0700

    Added filter dialog page

--- src/components/common/table/FilterDialog.js
+++ src/components/common/table/FilterDialog.js
@@ -1,16 +1,39 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
-import TextField from "@material-ui/core/TextField";
 import Dialog from "@material-ui/core/Dialog";
-import DialogActions from "@material-ui/core/DialogActions";
-import DialogContent from "@material-ui/core/DialogContent";
-import DialogTitle from "@material-ui/core/DialogTitle";
+import Icon from "@material-ui/core/Icon";
+import IconButton from "@material-ui/core/IconButton";
+import Tooltip from "@material-ui/core/Tooltip";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import CloseIcon from "@material-ui/icons/Close";
+import Typography from "@material-ui/core/Typography";
+import ListItem from "@material-ui/core/ListItem";
+import List from "@material-ui/core/List";
+import Divider from "@material-ui/core/Divider";
+import ListItemText from "@material-ui/core/ListItemText";
+import {withStyles} from "@material-ui/core/styles";
+import Slide from "@material-ui/core/Slide";
 
-export default class EditInfoButton extends React.Component {
+const styles = theme => ({
+    appBar: {
+        position: "relative"
+    },
+    title: {
+        marginLeft: theme.spacing.unit * 2,
+        flex: 1
+    }
+});
+
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+    return <Slide direction="up" ref={ref} {...props} />;
+});
+
+class FilterDialog extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            value: "",
             open: false
         };
     }
@@ -23,52 +46,55 @@ export default class EditInfoButton extends React.Component {
         this.setState({open: false});
     };
 
-    handleEdit = event => {
-        this.setState({value: event.target.value});
-    };
-
     render() {
+        const {classes} = this.props;
+
         return (
             <div>
-                <Button
-                    variant="outlined"
-                    color="primary"
-                    onClick={this.handleClickOpen}
-                >
-                    Edit
-                </Button>
-                <Dialog
-                    open={this.state.open}
-                    onClose={this.handleClose}
-                    aria-labelledby="form-dialog-title"
-                >
-                    <DialogTitle id="form-dialog-title">
-                        Change {this.props.desc}
-                    </DialogTitle>
-                    <DialogContent>
-                        <TextField
-                            autoFocus
-                            margin="dense"
-                            fullWidth
-                            onChange={this.handleEdit}
-                        />
-                    </DialogContent>
-                    <DialogActions>
-                        <Button onClick={this.handleClose} color="primary">
-                            Cancel
-                        </Button>
-                        <Button
-                            onClick={() => {
-                                this.props.handleChange(this.state.value);
-                                this.handleClose();
-                            }}
-                            color="secondary"
-                        >
-                            Done
-                        </Button>
-                    </DialogActions>
+                <Tooltip title="Filter">
+                    <IconButton aria-label="Filter" onClick={this.handleClickOpen}>
+                        <Icon>filter_list</Icon>
+                    </IconButton>
+                </Tooltip>
+                <Dialog fullScreen open={this.state.open} onClose={this.handleClose} TransitionComponent={Transition}>
+                    <AppBar className={classes.appBar}>
+                        <Toolbar>
+                            <IconButton
+                                edge="start"
+                                onClick={this.handleClose}
+                                aria-label="Close"
+                                color="inherit"
+                            >
+                                <CloseIcon/>
+                            </IconButton>
+                            <Typography
+                                color="inherit"
+                                variant="h6"
+                                className={classes.title}
+                            >
+                                Filters
+                            </Typography>
+                            <Button color="inherit" onClick={this.handleClose}>
+                                save
+                            </Button>
+                        </Toolbar>
+                    </AppBar>
+                    <List>
+                        <ListItem button>
+                            <ListItemText primary="Phone ringtone" secondary="Titania"/>
+                        </ListItem>
+                        <Divider/>
+                        <ListItem button>
+                            <ListItemText
+                                primary="Default notification ringtone"
+                                secondary="Tethys"
+                            />
+                        </ListItem>
+                    </List>
                 </Dialog>
             </div>
         );
     }
 }
+
+export default withStyles(styles)(FilterDialog);
